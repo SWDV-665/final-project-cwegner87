c6388c5679e8a091b6028fbd27c0ccccefa9e792
cwegner87 <cwegner1@live.maryville.edu>
2022-08-21 20:42:08 -0400

    adding the icons and alert

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {}
 
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title:'Alert',
+      subTitle: 'You are turning off the light',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
+
+
